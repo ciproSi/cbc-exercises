e89c6597c70ad41557a0f66c3142d3ddd3c0fe62
Šimon Cipro <simon.cipro@gmail.com>
2020-09-21 15:07:03 +0200

    basic JS exercises

---
+++ day-11/js-practice/js-practice-2/js/functions.js
@@ -0,0 +1,30 @@
+console.log('Correctly connected to HTML.');
+let state = 0;
+
+
+
+
+const set_headline = () => {
+    // let h1 = document.getElementById('headline');
+    // console.log(h1); //tohle vrátí celej ten řádek včetně elementu
+    // console.log(h1.innerHTML); //tohle vrátí obsah elementu
+
+    // //innerHTML můžu použít i pro zapsání
+    // // h1.innerHTML = "JavaScript";
+    
+    //vše předchozí do jednoho řádku:
+    
+    
+    if (state == 0) {
+            document.getElementById('headline').innerHTML = "JavaScript";
+            document.getElementById('headline').style.backgroundColor = "Red";
+            state = 1;
+    } else {
+            h1 = document.getElementById('headline');
+            h1.innerHTML = "I need to study. But what?"
+            h1.style.backgroundColor = "blue";
+
+            state = 0;
+        }
+
+}
---
+++ day-11/js-practice/js/script.js
@@ -0,0 +1,29 @@
+console.log('ahoj');
+
+
+let nrClicks = 0;
+
+
+// function nrClick () {
+//     nrClicks++;
+//     console.log('The button was pressed ' + ++nrClicks + ' times.');
+    
+//     ternary operator: if this is true  ? do this : otherwise do this
+
+//     let result = (1 == 2 ? "this is true" : "this is false");
+//     console.log(result);
+    
+    
+//     for (let i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+// arrow declaration of finction
+
+const nrClick = () => {
+    console.log('The button was pressed ' + ++nrClicks + ' times.');
+}
+
