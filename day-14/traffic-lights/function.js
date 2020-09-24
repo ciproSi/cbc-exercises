6e13109436f89ecbd90097342387e972b7680b05
Šimon Cipro <simon.cipro@gmail.com>
2020-09-24 21:45:29 +0200

    disable button when once pressed until red is on again

--- day-14/traffic-lights/function.js
+++ day-14/traffic-lights/function.js
@@ -3,13 +3,43 @@
 //get the elements
 const stopLight = document.querySelector('#stopLight'),
       walkLight = document.querySelector('#walkLight'),
-      btn = document.querySelector('#desireToGo');
+      btn = document.querySelector('#desireToGo'),
+      timerElm = document.getElementById('timer');
 
 btn.addEventListener('click', () => {
+    // disable button for until stop sign is on again
+    btn.disabled = true;
+   
+    //get the random value from 2-5s for pedestrians waiting time
+    const waitingTime = Math.floor(Math.random() * 3000) + 2000; 
+
+    //get the times in seconds for counter
+    let timer = Math.floor(waitingTime / 1000) + 1;
+    
+    timerElm.textContent = timer + " s";
+    const timerID = setInterval(() => {
+        timer -= 1;
+        timerElm.textContent = timer + " s";
+    }, 1000);
+
+    //change light to walk-on after random waiting time + kill the waiting countdown
     setTimeout(() => {
         stopLight.setAttribute("src", "stop-off.svg");
         walkLight.setAttribute("src", "walk-on.svg");
-    }, 1000);
+        clearInterval(timerID);
+        timerElm.innerHTML = "";
+    }, waitingTime);
+
+    setTimeout(() => {
+        stopLight.setAttribute("src", "stop-on.svg");
+        walkLight.setAttribute("src", "walk-off.svg");
+        
+        //enable the button again
+        btn.disabled = false;
+    }, waitingTime + 5000);
+    
 });
+
+
 
 
