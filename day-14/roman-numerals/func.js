b51a1c41c6efaeae3e99b3f3a967efdfe88aef16
Šimon Cipro <simon.cipro@gmail.com>
2020-09-25 11:17:14 +0200

    different versions of morning workout

--- day-14/roman-numerals/func.js
+++ day-14/roman-numerals/func.js
@@ -3,16 +3,22 @@
 let usrInput = Number(prompt('Insert arabic number.'));
 let result = "";
 
-if (usrInput === 900) {
-    result = "CM";
-    usrInput = 0;
+// if (usrInput === 900) {
+//     result = "CM";
+//     usrInput = 0;
+// }
+console.log(usrInput);
+if (usrInput >= 1000) {
+    const m = Math.floor(usrInput / 1000);
+    for (let i = 0; i < m; i++) { result += "M"; };
 }
 
-const m = Math.floor(usrInput / 1000);
-for (let i = 0; i < m; i++) { result += "M"; };
+if (usrInput % 1000 >= 900) {
+    result += "CM";
+}
 
 
-const d = Math.floor((usrInput % 1000) / 500);
-for (let i = 0; i < d; i++) { result += "D"};
+// const d = Math.floor((usrInput % 1000) / 500);
+// for (let i = 0; i < d; i++) { result += "D"};
 
-console.log(result, m, d);
+console.log(result);
--- day-15/workout/func.js
+++ day-15/workout/func.js
@@ -1,28 +1,54 @@
 'use strict';
 
-const padllockImg = document.querySelector('img'),
-      container = document.getElementById('hidden-message');
+// // preserves the message within the html, use .toggle
+    // const padllockImg = document.querySelector('img');
+
+    // const toggleVisible = () => {
+    //     const message = document.getElementById('message');
+    //     message.classList.toggle('hidden');
+    // }
+
+    // padllockImg.addEventListener('mouseenter', toggleVisible);
+    // padllockImg.addEventListener('mouseleave', toggleVisible);
 
 
-// this way still preserves the message in HTML
-// padllockImg.addEventListener('mouseenter', () => {
-//     message.style = "display:none";
-// })
+// // this way still preserves the message in HTML
+    // const message = document.getElementById('message'),
+    //       padllockImg = document.querySelector('img');
+    
+    // padllockImg.addEventListener('mouseenter', () => {
+    //     message.style = "display:none";
+    // });
 
-// padllockImg.addEventListener('mouseleave', () => {
-//     message.style = "display: block";
-// });
+    // padllockImg.addEventListener('mouseleave', () => {
+    //     message.style = "display: block";
+    // });
+
+// // like with .toggle but with classList.add and classList.remove
+    const message = document.getElementById('message'),
+          padllockImg = document.querySelector('img');
+    
+    padllockImg.addEventListener('mouseenter', () => {
+        message.classList.add('hidden');
+    });
+
+    padllockImg.addEventListener('mouseleave', () => {
+        message.classList.remove('hidden');
+    });
 
 
-//this way the message dissapears from html - bit better
-padllockImg.addEventListener('mouseenter', () => {
-    const message = document.getElementById('message');
-    message.parentNode.removeChild(message);
-})
+// //this way the message dissapears from html - bit better
+    // const padllockImg = document.querySelector('img'),
+    //       container = document.getElementById('hidden-message');
 
-padllockImg.addEventListener('mouseleave', () => {
-    const messageRe = document.createElement('p');
-    messageRe.setAttribute('id', 'message');
-    messageRe.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo exercitationem odio iure molestiae maxime sint voluptatum, earum laudantium nemo excepturi officiis esse harum. Unde totam fugit quo consequatur voluptas! Aperiam.";
-    container.appendChild(messageRe);
-})
+    // padllockImg.addEventListener('mouseenter', () => {
+    //     const message = document.getElementById('message');
+    //     message.parentNode.removeChild(message);
+    // });
+
+    // padllockImg.addEventListener('mouseleave', () => {
+    //     const messageRe = document.createElement('p');
+    //     messageRe.setAttribute('id', 'message');
+    //     messageRe.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo exercitationem odio iure molestiae maxime sint voluptatum, earum laudantium nemo excepturi officiis esse harum. Unde totam fugit quo consequatur voluptas! Aperiam.";
+    //     container.appendChild(messageRe);
+    // });
