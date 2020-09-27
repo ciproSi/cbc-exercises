dc6262b18a22f3226330e9a9a1d54c356c12808e
Šimon Cipro <simon.cipro@gmail.com>
2020-09-27 22:08:57 +0200

    basic mobile menu functionality addedů

--- day-9/ania-webia-new/src/js/index.js
+++ day-9/ania-webia-new/src/js/index.js
@@ -2,3 +2,29 @@
 
 console.log('conneceted to index.js');
 
+//get the hamburger icon, page container, mobile menu and close mobile menu icon elements
+const hamburgerBtn = document.getElementById('hamburger'),
+      pageContainer = document.querySelector('.page'),
+      mobileMenuContainer = document.querySelector('.mobile-menu'),
+      closeMobMenu = document.getElementById('closeMobMenu');
+
+hamburgerBtn.addEventListener('click', () => {
+    
+    //display mobile menu by adding the class
+    mobileMenuContainer.classList.toggle('mobile-menu--display');
+
+    //hide the all page content so only mobile menu stays visible
+    pageContainer.classList.toggle('page--hidden');
+    
+});
+
+closeMobMenu.addEventListener('click', () => {
+    pageContainer.classList.toggle('page--hidden');
+    mobileMenuContainer.classList.toggle('mobile-menu--display');
+});
+
+const mobileMenuLinks = document.querySelector('.mobile.menu a');
+mobileMenuLinks.addEventListener('click', () => {
+    pageContainer.classList.toggle('page--hidden');
+    mobileMenuContainer.classList.toggle('mobile-menu--display');
+});
