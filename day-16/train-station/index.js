a1a9eb800c90ad46065043a523949e958d53a49b
Šimon Cipro <simon.cipro@gmail.com>
2020-09-28 17:38:30 +0200

    train station table done

--- day-16/train-station/index.js
+++ day-16/train-station/index.js
@@ -1,21 +1,66 @@
 'use strict';
 
-const departures = [494, 499, 500, 517, 520, 523, 530, 556, 563, 576, 586, 613, 625, 175];
+//time of departure is stored as a minutes before midnight
+const departures = [
+    {time: 494,
+     train: "Acela Express",
+     no: 2151,
+     to: "Horní Planá",
+     status: "on time",
+     track: 14,
+    },
+    {time: 499,
+        train: "New Heaven Line",
+        no: 1542,
+        to: "Černé Hory",
+        status: "on time",
+        track: 10,
+    },
+    {time: 517,
+        train: "Shore Line East",
+        no: 25,
+        to: "Horní Planá",
+        status: "delayed",
+        track: 8,
+    },
+    {time: 517,
+        train: "Shore Line East",
+        no: 25,
+        to: "Horní Planá",
+        status: "delayed",
+        track: 8,
+    },
+    {time: 517,
+        train: "Shore Line East",
+        no: 25,
+        to: "Horní Planá",
+        status: "delayed",
+        track: 8,
+    },
 
-//get the element to display
-const departuresElm = document.querySelector('.departures');
+];
 
+//get the elements to display
+const departureTableElm = document.querySelector('.departure-table');
+
+//display data to the table
 let timeOfDeparture;
-departures.forEach((time) => {
+departures.forEach((train) => {
     
     //put 0 in front of minute if there is less then 10 minutes
-    if (60 - time % 60 < 10) {
-        timeOfDeparture = 23 - (Math.floor(time / 60)) + ":0" + (60 - time % 60);
+    if (60 - train.time % 60 < 10) {
+        timeOfDeparture = 23 - (Math.floor(train.time / 60)) + ":0" + (60 - train.time % 60);
     } else {
-        timeOfDeparture = 23 - (Math.floor(time / 60)) + ":" + (60 - time % 60);
+        timeOfDeparture = 23 - (Math.floor(train.time / 60)) + ":" + (60 - train.time % 60);
     };
-    departuresElm.innerHTML += `
-        <li>${timeOfDeparture}</li>
+    departureTableElm.innerHTML += `
+        <tr class="table-row">
+            <td>${timeOfDeparture}</td>
+            <td>${train.train}</td>
+            <td>${train.no}</td>
+            <td>${train.to}</td>
+            <td>${train.status}</td>
+            <td>${train.track}</td>
+        </tr>
     `;
 });
-
