98eb4e98b53a18f4f4ba86f728f9e798af49910c
Šimon Cipro <simon.cipro@gmail.com>
2020-09-29 12:55:18 +0200

    added delete btn to chat

--- day-17/workout/index.js
+++ day-17/workout/index.js
@@ -9,12 +9,21 @@ conversation.forEach((message,index) => {
                 <img src="${message.name.toLowerCase()}.png" alt="">
                 <div class="message__self--${message.side}">${message.text}</div>
                 <button class="btn-like"></button>
+                <button class="btn-delete"></button>
             `;
+            //add like
             const likeBtnElm = msg.querySelector('.btn-like');
             let likesAmount = 0;
             likeBtnElm.addEventListener('click', () => {
                 likeBtnElm.innerHTML = ++likesAmount; 
             });
+
+            //delete message
+            const deleteBtnElm = msg.querySelector('.btn-delete');
+            deleteBtnElm.addEventListener('click', () => {
+                containerElm.removeChild(msg);
+            });
+
             containerElm.appendChild(msg);
     }, index * 800);
 });    
