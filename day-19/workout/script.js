c7f3340c61dd82f561fdcbb73ea77121092a1b98
Šimon Cipro <simon.cipro@gmail.com>
2020-10-01 10:29:08 +0200

    day-19 workout done w/ class

--- day-19/workout/script.js
+++ day-19/workout/script.js
@@ -1,10 +1,41 @@
 'use strict';
 
-const starsAll = document.querySelectorAll('.rating__star');
+class Rating {
+    constructor(ratingValue) {
+        this.ratingValue = ratingValue;
+    }
 
-starsAll.forEach((star, index) => {
-    star.addEventListener('click', () => {
-        
+    render () {
+        let element = document.createElement('div');
+        element.className = 'rating';
+        element.innerHTML = `
+            <div class="rating__value"></div>
+            <div class="rating__stars">
+                <div class="rating__star rating__star--on"></div>
+                <div class="rating__star rating__star--on"></div>
+                <div class="rating__star rating__star--on"></div>
+                <div class="rating__star"></div>
+                <div class="rating__star"></div>
+            </div>
+        `;
+        const starsAll = element.querySelectorAll('.rating__star');
+        starsAll.forEach((star, index) => {
+            star.addEventListener('click', () => {
+                this.update(index);
+            });
+        });
+        return element;
+    }
+
+    mount(parent) {
+        this.element = this.render();
+        parent.appendChild(this.element);
+        this.update(this.ratingValue - 1);
+    }
+
+    update(index) {
+        console.log(index);
+        const starsAll = this.element.querySelectorAll('.rating__star');
         starsAll.forEach((star) => {
             star.classList.remove('rating__star--on');
         });
@@ -13,9 +44,16 @@ starsAll.forEach((star, index) => {
             starsAll[i].classList.add('rating__star--on');
         };
 
-        const ratingValue = document.querySelector('.rating__value');
+        const ratingValue = this.element.querySelector('.rating__value');
         ratingValue.textContent = index + 1;
-    });
-});
+    }
+}
 
 
+
+
+const testElm = document.querySelector('.test');
+let rating1 = new Rating(5);
+let rating2 = new Rating(3);
+rating1.mount(testElm);
+rating2.mount(testElm);
