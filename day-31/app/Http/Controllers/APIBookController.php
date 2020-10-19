bdaff72c72bc161ad60e30911ba8ebe06d9bedeb
Šimon Cipro <simon.cipro@gmail.com>
2020-10-19 11:19:09 +0200

    day-31 morning workout

---
+++ day-31/app/Http/Controllers/APIBookController.php
@@ -0,0 +1,25 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+ 
+class APIBookController extends Controller
+{
+    public function index()
+    {
+        $page_nr = $_GET['page'] ?? 1;
+
+
+        $query = '
+            SELECT *
+            FROM `books`
+            WHERE 1
+            LIMIT ' . ($page_nr - 1) * 4 . ', 4
+        ';
+        $books = \DB::select($query);
+        
+
+        return $books;
+    }
+}
